6f58096872b299ac4f6d1ffbb5feead63671c18f
Daiana <dauipoma@gmail.com>
2023-04-26 07:29:11 +0200

    Add Card component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductCard from './components/ProductCard.vue';
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <HelloWorld msg="Post It!" />
-    </div>
+    <h1 class="text font-bold text-center text-4xl py-2">Product</h1>
   </header>
+  <main class="grid grid-cols-2 lg:grid-cols-4 gap-3 px-3">
+    <ProductCard title="Magic Socks" materials="50% cotton" price="15"/>
+    <ProductCard title="Black Jeans" materials="80% cotton" price="26"/>
+    <ProductCard title="White Jumper" materials="80% cotton" price="37"/>
+    <ProductCard title="Blue Jean Jacket" materials="80% cotton" price="41"/>
+    <div>Total: $0</div>
+  </main>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+    title: String,
+    materials: String,
+    price:Number
+})
+
+const counter = ref(0)
+
+</script>
+
+<template>
+<div class="text-green-600 rounded-md border-2 border-gray-300 p-3 text-lg">
+    <h2 class="font-semibold text-3xl">{{ title }}</h2>
+    <span>${{ price }}</span>
+    <p>{{ materials }}</p>
+    <div v-if="counter > 0">
+        <button class="border px-3 font-semibold" @click="() => counter--">-</button>
+        <span class="px-2">{{counter }}</span> 
+        <button class="border px-3 font-semibold" @click="() => counter++">+</button>
+    </div>
+    <button v-else @click="counter = 1" class="px-3 py-1 border-2 rounded-sm">Add to Cart</button>
+</div>
+</template>
